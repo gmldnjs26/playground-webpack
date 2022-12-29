e2a8f810dbcf7cd2acee2e3b9f42db0e515915af
gmldnjs26 <imleesky265@gmail.com>
2022-12-29 16:55:50 +0900

    feat: use sass postcss

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from "lodash";
+import "./test.scss";
+import "./test2.scss";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.className = "container";
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,4 +7,43 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 따로 css 파일을 빼는 loader
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          {
+            loader: "css-loader",
+          },
+          // autoprefixer를 쓰기 위한 postcss loader
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require("autoprefixer")({
+                    flexbox: true,
+                    grid: true,
+                    overrideBrowserslist: ["last 3 versions", "> 1%"],
+                  }),
+                ],
+              },
+            },
+          },
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style/app.css",
+    }),
+  ],
 };
